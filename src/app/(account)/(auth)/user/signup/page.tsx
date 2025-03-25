e2c9912712d22890bb81f1ffa0e70038aab8c935
parkjungkwan <pakjkwan@gmail.com>
2025-03-25 16:18:20 +0900

    .

--- src/app/(account)/(auth)/user/signup/page.tsx
+++ src/app/(account)/(auth)/user/signup/page.tsx
@@ -1,6 +1,5 @@
-export const dynamic = "force-dynamic"
-
 "use client";
+export const dynamic = "force-dynamic"
 // 클라이언트 컴포넌트로 선언
 import SignupForm from "@/components/(account)/auth/user/signup/SignupForm";
 
